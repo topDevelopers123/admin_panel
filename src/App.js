439ce6b5808a4ce0acc57a337628b6ba6536a111
topDevelopers123 <jhahimanshu498@gmail.com>
2024-06-17 18:48:28 +0530

    created some pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Header from './admin_Panel/header/Header'; 
 import Add_new_banner from './admin_Panel/banner/Add_new_banner';
 import Category from './admin_Panel/category/main_category/Category';
+import Sub_category from './admin_Panel/category/sub_category/Sub_category';
+import Sub_inner_category from './admin_Panel/category/sub_inner_category/Sub_inner_category';
+import Category_table from './admin_Panel/category/main_category/Category_table';
+import Sub_category_table from './admin_Panel/category/sub_category/Sub_category_table';
+import Sub_inner_category_table from './admin_Panel/category/sub_inner_category/Sub_inner_category_table';
+// import Order from './admin_Panel/order/Order';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Add_new_product from './admin_Panel/product/Add_new_product'
@@ -13,9 +19,7 @@ function App() {
  <>
   <div>
     <Header/>
-    <Add_new_product/>
-        
-        <Category />
+
   </div>
  </>
   );
---
+++ src/admin_Panel/order/order.js
@@ -0,0 +1,211 @@
+import React from 'react'
+
+function Order() {
+    return (
+        <div>
+            <div class="page-wrapper">
+                <div class="page-content">
+                    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                        <div class="breadcrumb-title pe-3">Mayavi Fashion</div>
+                        <div class="ps-3">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb mb-0 p-0">
+                                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">Orders</li>
+                                </ol>
+                            </nav>
+                        </div>
+
+                    </div>
+
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-lg-flex align-items-center mb-4 gap-3">
+                                <div class="position-relative">
+                                    <input type="text" class="form-control ps-5 radius-30" placeholder="Search Order" /> <span class="position-absolute top-50 product-show translate-middle-y"><i class="bx bx-search"></i></span>
+                                </div>
+
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table mb-0">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th>Product Image</th>
+                                            <th>Title </th>
+                                            <th>Category </th>
+                                            <th>Username </th>
+                                            <th>Address </th>
+                                            <th>Order placed at </th>
+                                            <th>Payment Status</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="ms-2">
+                                                    <img src="assets/images/category/cat_img/men.jpg" alt="img" />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Polo Shirt
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Men
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                   Nikhil
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    9571 Gottlieb Crest, South Darinland, WA 40656
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    16th May 2024
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-danger bg-light-danger p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>Pending</div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-success bg-light-success p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>FulFilled</div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="ms-2">
+                                                    <img src="assets/images/category/cat_img/men.jpg" alt="img" />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Polo Shirt
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Men
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                   Nikhil
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    9571 Gottlieb Crest, South Darinland, WA 40656
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    16th May 2024
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-danger bg-light-danger p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>Pending</div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-success bg-light-success p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>FulFilled</div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="ms-2">
+                                                    <img src="assets/images/category/cat_img/men.jpg" alt="img" />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Polo Shirt
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Men
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                   Nikhil
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    9571 Gottlieb Crest, South Darinland, WA 40656
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    16th May 2024
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-danger bg-light-danger p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>Pending</div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-success bg-light-success p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>FulFilled</div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div class="ms-2">
+                                                    <img src="assets/images/category/cat_img/men.jpg" alt="img" />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Polo Shirt
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    Men
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                   Nikhil
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    9571 Gottlieb Crest, South Darinland, WA 40656
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="ms-2">
+                                                    16th May 2024
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-danger bg-light-danger p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>Pending</div>
+                                            </td>
+                                            <td>
+                                                <div class="badge rounded-pill text-success bg-light-success p-2 text-uppercase px-3"><i class='bx bxs-circle me-1'></i>FulFilled</div>
+                                            </td>
+                                        </tr>
+                                       
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Order
