b97b6bd2f79f2e57d0617cab6fca2f33435466c0
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-06-18 11:22:22 +0530

    cnages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import './App.css';
 import Header from './admin_Panel/header/Header'; 
 import Add_new_banner from './admin_Panel/banner/Add_new_banner';
@@ -9,9 +12,9 @@ import Category_table from './admin_Panel/category/main_category/Category_table'
 import Sub_category_table from './admin_Panel/category/sub_category/Sub_category_table';
 import Sub_inner_category_table from './admin_Panel/category/sub_inner_category/Sub_inner_category_table';
 // import Order from './admin_Panel/order/Order';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import Add_new_product from './admin_Panel/product/Add_new_product'
+import Product_details from './admin_Panel/product/Product_details';
 
 
 function App() {
@@ -19,9 +22,7 @@ function App() {
  <>
   <div>
     <Header/>
-   
-
-        <Category />
+    <Product_details />
  
 
   </div>
