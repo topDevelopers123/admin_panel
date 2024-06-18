3d4115e4f8b9822acb65b2274487c8f4993e892c
topDevelopers123 <jhahimanshu498@gmail.com>
2024-06-18 18:40:33 +0530

    password pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './admin_Panel/header/Header'; 
+import Header from './admin_Panel/header/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Banner from './admin_Panel/banner/Banner';
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 
 
 function App() {
   return (
- <>
-  <div>
-    <Header/>
-    <Banner/>
-  </div>
- </>
+    <>
+      <div>
+        {/* <Header/> */}
+
+      </div>
+    </>
   );
 }
 
